2864bc42e00e54f6cd05dda1c6c9936f5481b0fa
JustOff <Off.Just.Off@gmail.com>
2017-10-25 20:11:13 +0300

    Update the magic

--- bootstrap.js
+++ bootstrap.js
@@ -8,7 +8,21 @@ let Observer = {
 		try {
 			if (topic == 'http-on-modify-request' && subject instanceof Ci.nsIHttpChannel) {
 				if (subject.URI.host.match(/\.photobucket\.com$/)) {
-					subject.setRequestHeader("Referer", subject.URI.spec, false);
+					let user;
+					if ((user = /albums\/[^\/]+\/([^\/]+)\//.exec(subject.URI.spec)) !== null) {
+						let url = subject.URI.QueryInterface(Ci.nsIURL);
+						if (url.query.indexOf("hotlinkfix") == -1) {
+							let query, date = new Date().getTime();
+							if (url.query == "") {
+								query = "?hotlinkfix=" + date;
+							} else {
+								query = url.query + "&hotlinkfix=" + date;
+							}
+							subject.redirectTo(Services.io.newURI(subject.URI.spec + query, null, null));
+						} else {
+							subject.setRequestHeader("Referer", subject.URI.scheme + '://photobucket.com/gallery/user/' + user[1] + '/media/', false);
+						}
+					}
 				}
 			}
 		} catch (e) {}
